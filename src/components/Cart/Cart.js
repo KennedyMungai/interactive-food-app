5767088884296c5b619e5117a3a99be1e546e765
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-17 20:44:20 +0300

    Awaited the fetch function call

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -35,7 +35,7 @@ const Cart = (props) =>
   {
     setIsSubmitting(true)
 
-    fetch('https://simple-react-backend-default-rtdb.firebaseio.com/orders.json', {
+    await fetch('https://simple-react-backend-default-rtdb.firebaseio.com/orders.json', {
       method: 'POST',
       body: JSON.stringify({
         user: userData,
