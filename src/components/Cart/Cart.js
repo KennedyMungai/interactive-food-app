783247b28027686fdb39efa3619eb1426be8583d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-17 09:33:53 +0300

    Added an import for the checkout component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,18 +4,22 @@ import Modal from '../UI/Modal';
 import CartItem from './CartItem';
 import classes from './Cart.module.css';
 import CartContext from '../../store/cart-context';
+import Checkout from './Checkout';
 
-const Cart = (props) => {
+const Cart = (props) =>
+{
   const cartCtx = useContext(CartContext);
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
-  const cartItemRemoveHandler = (id) => {
+  const cartItemRemoveHandler = (id) =>
+  {
     cartCtx.removeItem(id);
   };
 
-  const cartItemAddHandler = (item) => {
+  const cartItemAddHandler = (item) =>
+  {
     cartCtx.addItem(item);
   };
 
