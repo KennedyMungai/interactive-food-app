eca8b3c3c904ef9b61dc47a7c2e9bb730e7ade1f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-17 20:55:42 +0300

    Rendered out the const

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -84,13 +84,7 @@ const Cart = (props) =>
 
   return (
     <Modal onClose={props.onClose}>
-      {cartItems}
-      <div className={classes.total}>
-        <span>Total Amount</span>
-        <span>{totalAmount}</span>
-      </div>
-      {isCheckout && <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose} />}
-      {!isCheckout && modalActions}
+      {cartModalContent}
     </Modal>
   );
 };
