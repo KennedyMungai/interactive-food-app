175d15055b980a1aa0cc7d93b700a052349beb50
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-17 15:04:39 +0300

    Added some dynamic classes for input validation for the form

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -48,6 +48,9 @@ const Checkout = (props) =>
     }
 
     const nameControlClasses = `${classes.control} ${formInputsValidity.name ? '' : classes.invalid}`
+    const streetControlClasses = `${classes.control} ${formInputsValidity.street ? '' : classes.invalid}`
+    const postalControlClasses = `${classes.control} ${formInputsValidity.postalCode ? '' : classes.invalid}`
+    const cityControlClasses = `${classes.control} ${formInputsValidity.city ? '' : classes.invalid}`
 
     return (
         <form onSubmit={confirmHandler}>
@@ -56,17 +59,17 @@ const Checkout = (props) =>
                 <input type="text" id="name" ref={nameInputRef} />
                 {!formInputsValidity.name && <p>Please enter a valid name!</p>}
             </div>
-            <div className={nameControlClasses}>
+            <div className={streetControlClasses}>
                 <label htmlFor="street">Street</label>
                 <input type="text" id="street" ref={streetInputRef} />
                 {!formInputsValidity.street && <p>Please enter a valid street name!</p>}
             </div>
-            <div className={nameControlClasses}>
+            <div className={postalControlClasses}>
                 <label htmlFor="postal">Postal Code</label>
                 <input type="text" id="postal" ref={postalCodeInputRef} />
                 {formInputsValidity.postalCode && <p>Please enter a valid postal code</p>}
             </div>
-            <div className={nameControlClasses}>
+            <div className={cityControlClasses}>
                 <label htmlFor="city">City</label>
                 <input type="text" id="city" ref={cityInputRef} />
                 {formInputsValidity.city && <p>Please enter a valid city</p>}
