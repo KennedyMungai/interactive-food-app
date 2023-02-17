4b1964534ed8201b5628751bd2a7d02eba833a21
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-17 09:31:20 +0300

    Defined the form for the Checkout component

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -4,7 +4,20 @@ import classes from './Checkout.module.css'
 const Checkout = () =>
 {
     return (
-        <div>Checkout</div>
+        <form>
+            <div className={classes.control}>
+                <label htmlFor="name">Your Name</label>
+                <input type="text" id="name" />
+            </div>
+            <div className={classes.control}>
+                <label htmlFor="street">Street</label>
+                <input type="text" id="street" />
+            </div>
+            <div className={classes.control}>
+                <label htmlFor="postal">Postal Code</label>
+                <input type="text" id="postal" />
+            </div>
+        </form>
     )
 }
 
