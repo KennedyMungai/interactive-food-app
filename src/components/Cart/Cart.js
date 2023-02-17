871c391f5bccf8c05e10412ae2ab5bec8049e5ea
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-17 10:31:43 +0300

    Hid the order and close buttons when the isCheckout state is set to false

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -53,12 +53,15 @@ const Cart = (props) =>
         <span>{totalAmount}</span>
       </div>
       {isCheckout && <Checkout />}
-      <div className={classes.actions}>
-        <button className={classes['button--alt']} onClick={props.onClose}>
-          Close
-        </button>
-        {hasItems && <button className={classes.button} onClick={orderHandler}>Order</button>}
-      </div>
+      {
+        !isCheckout &&
+        <div className={classes.actions}>
+          <button className={classes['button--alt']} onClick={props.onClose}>
+            Close
+          </button>
+          {hasItems && <button className={classes.button} onClick={orderHandler}>Order</button>}
+        </div>
+      }
     </Modal>
   );
 };
